9c1bd4379d3bdd3d5d86de419f07cb5e20e0db27
Sam Bell <sambelldev@tuta.io>
2021-10-20 12:05:47 +0100

    Breaking for lunch commit, hashing is present but SQL query still broken

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mysql = require('promise-mysql')
 const cors = require('cors')
 const session = require('express-session');
+const bcrypt = require('bcryptjs')
 const app = express()
 const port = 3001
 const oneDay = 1000 * 60 * 60 * 24;
@@ -9,6 +10,7 @@ const oneDay = 1000 * 60 * 60 * 24;
 app.use(cors())
 app.use(express.json());
 app.set('trust proxy', 1)
+
 app.use(session({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
     saveUninitialized:true,
@@ -16,17 +18,31 @@ app.use(session({
     resave: false
 }));
 
-app.post('/login', async (req, res) => {
-
-    const isDoctor = req.body.isDoctor
-    const emailEntered = req.body.email
-    const passwordEntered = req.body.password
+app.post('/register', async (req, res) => {
 
     const connection = await mysql.createConnection({
         user: 'root',
         password: 'password',
         database: 'lrss_2021-10-18'
     })
+
+    try {
+        const userEmail = req.body.email
+        const userPassword = req.body.password
+        const hash = await bcrypt.hash(userPassword, 12)
+        await connection.query("INSERT INTO `patients` (`email`, `hash`) VALUES ('email: " + userEmail + "', hash: " + hash + "');")
+        res.status(200).send('Registration complete')
+    } catch(e) {
+        console.log(e)
+        res.status(500).send('Registration failed')
+    }
+})
+
+app.post('/login', async (req, res) => {
+
+    const isDoctor = req.body.isDoctor
+    const emailEntered = req.body.email
+    const passwordEntered = req.body.password
 
     const checkLogin = async (databaseName) => {
         const userData = await connection.query("SELECT `email`, `password` FROM `" + databaseName + "` WHERE `email` = '" + emailEntered + "';")
