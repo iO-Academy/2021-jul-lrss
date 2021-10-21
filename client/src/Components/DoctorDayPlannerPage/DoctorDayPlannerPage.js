312ca4e6587a96e713c34421e9216f97d799b399
Rosa <rosa@droogers.nl>
2021-10-21 21:41:11 +0100

    displayedayoverview

--- client/src/Components/DoctorDayPlannerPage/DoctorDayPlannerPage.js
+++ client/src/Components/DoctorDayPlannerPage/DoctorDayPlannerPage.js
@@ -7,6 +7,16 @@ const DoctorDayPlannerPage = () => {
     const [dateSelected, setDateSelected] = useState(new Date())
     // const [appointmentDetails, setAppointmentDetails] = useState([])
     const [dayOverview, setDayOverview] = useState(
+        {
+            1: {},
+            2: {},
+            3: {},
+            4: {},
+            5: {},
+            6: {},
+            7: {},
+            8: {}
+        }
     )
 
     const displayTimeSlot = (time_slot) => {
@@ -47,101 +57,31 @@ const DoctorDayPlannerPage = () => {
             .then(response => response.json())
             .then(data => {
                 if(data.length > 0) {
-                    console.log('hi')
-                    const appointment1 = data.filter(appointment => appointment.time_slot === 1)
-                    const appointment2 = data.filter(appointment => appointment.time_slot === 2)
-                    const appointment3 = data.filter(appointment => appointment.time_slot === 3)
-                    const appointment4 = data.filter(appointment => appointment.time_slot === 4)
-                    const appointment5 = data.filter(appointment => appointment.time_slot === 5)
-                    const appointment6 = data.filter(appointment => appointment.time_slot === 6)
-                    const appointment7 = data.filter(appointment => appointment.time_slot === 7)
-                    const appointment8 = data.filter(appointment => appointment.time_slot === 8)
-                    console.log(appointment1)
-                    data = [
-                        appointment1,
-                        appointment2,
-                        appointment3,
-                        appointment4,
-                        appointment5,
-                        appointment6,
-                        appointment7,
-                        appointment8
-                    ]
-                    setDayOverview(data)
+                    const appointmentData = {
+                        1: {},
+                        2: {},
+                        3: {},
+                        4: {},
+                        5: {},
+                        6: {},
+                        7: {},
+                        8: {}
+                    }
+                    data.forEach(appointment => {
+                        appointmentData[appointment.time_slot] = appointment
+                    })
+                    setDayOverview(appointmentData)
                 }
             })
             .catch(error => console.log(error))
     }, [dateSelected])
 
-    useEffect(() => {
-        console.log('dayoverview', dayOverview)
-    }, [dateSelected])
-
-    // useEffect(() => {
-    //    if(dayOverview[0].doctor !== '') {
-    //
-    //        let appointmentSlot1 = ''
-    //            dayOverview.forEach(appointment => {
-    //            if (appointment.time_slot == 1) {
-    //                appointmentSlot1 = appointment.patient
-    //            }
-    //        })
-    //    }
-    // }, [dayOverview])
-
     return (
         <div>
             <DatePicker setDateSelected={setDateSelected}/>
-            <div>
-                {/*<p>{dayOverview[0][0]}</p>*/}
-                {/*<p value={"{dayOverview[0][0]}"}></p>*/}
-                {/*<p>{dayOverview[0][3]}</p>*/}
-                {/*<p>{dayOverview[0][2]}</p>*/}
-                {/*<p>{dayOverview[2][2]}</p>*/}
-                {/*<p>{dayOverview[5][10]}</p>*/}
-                {/*<table>*/}
-                {/*    <tr>*/}
-                {/*        <td>9:00</td>*/}
-                {/*        <td>{appointmentDetails[0]}</td>*/}
-                {/*        <p>reason for visit</p>*/}
-                {/*    </tr>*/}
-                {/*    <tr>*/}
-                {/*        <td>10:00</td>*/}
-                {/*        <td>{appointmentDetails[1]}</td>*/}
-                {/*        <p>reason for visit</p>*/}
-                {/*    </tr>*/}
-                {/*    <tr>*/}
-                {/*        <td>11:00</td>*/}
-                {/*        <td>{appointmentDetails[3]}</td>*/}
-                {/*        <p>reason for visit</p>*/}
-                {/*    </tr>*/}
-                {/*    <tr>*/}
-                {/*        <td>12:00</td>*/}
-                {/*        <td>{appointmentDetails[4]}</td>*/}
-                {/*        <p>reason for visit</p>*/}
-                {/*    </tr>*/}
-                {/*    <tr>*/}
-                {/*        <td>13:00</td>*/}
-                {/*        <td>{appointmentDetails[5]}</td>*/}
-                {/*        <p>reason for visit</p>*/}
-                {/*    </tr>*/}
-                {/*    <tr>*/}
-                {/*        <td>14:00</td>*/}
-                {/*        <td>{appointmentDetails[6]}</td>*/}
-                {/*        <p>reason for visit</p>*/}
-                {/*    </tr>*/}
-                {/*    <tr>*/}
-                {/*        <td>15:00</td>*/}
-                {/*        <td>{appointmentDetails[7]}</td>*/}
-                {/*        <p>reason for visit</p>*/}
-                {/*    </tr>*/}
-                {/*    <tr>*/}
-                {/*        <td>16:00</td>*/}
-                {/*        <td>{getAppointmentDetails(8)}</td>*/}
-                {/*        <p>reason for visit</p>*/}
-                {/*    </tr>*/}
-                {/*</table>*/}
-            </div>
+                <table>
+                    <tableRow dayOverview={dayOverview} displayTimeSlot={displayTimeSlot}/>
+                </table>
         </div>
     )
 }
---
+++ client/src/Components/DoctorDayPlannerPage/dayOverviewTableRow.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from "react";
+
+const tableRow = (props) => {
+    console.log(props.dayOverview)
+    render(
+        <p>fdsd</p>
+    )
+}
+
+export default tableRow
