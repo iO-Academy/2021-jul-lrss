3f5b880160776d0dff0ccefaba2e0edae1d008ce
Rosa <rosa@droogers.nl>
2021-10-22 09:31:17 +0100

    changed functionality doctor page

--- client/src/Components/DoctorDayPlannerPage/DoctorDayPlannerPage.js
+++ client/src/Components/DoctorDayPlannerPage/DoctorDayPlannerPage.js
@@ -12,7 +12,7 @@ const DoctorDayPlannerPage = () => {
     const [moduleDisplay, setModuleDisplay] = useState(false)
     // const [appointmentDetails, setAppointmentDetails] = useState([])
     const [dayOverview, setDayOverview] = useState(
-        {
+            {
             1: {},
             2: {},
             3: {},
@@ -25,15 +25,16 @@ const DoctorDayPlannerPage = () => {
     )
 
     const initialStateDayOverview = {
-        1: {},
-        2: {},
-        3: {},
-        4: {},
-        5: {},
-        6: {},
-        7: {},
-        8: {}
-    }
+            1: {},
+            2: {},
+            3: {},
+            4: {},
+            5: {},
+            6: {},
+            7: {},
+            8: {}
+        }
+
 
     const displayTimeSlot = (time_slot) => {
         switch (time_slot){
@@ -46,13 +47,13 @@ const DoctorDayPlannerPage = () => {
             case 4:
                 return '12:00 noon'
             case 5:
-                return '13:00 pm'
+                return '01:00 pm'
             case 6:
-                return '14:00 pm'
+                return '02:00 pm'
             case 7:
-                return '15:00 pm'
+                return '03:00 pm'
             case 8:
-                return '16:00 pm'
+                return '04:00 pm'
             default:
                 return 'none'
         }
@@ -71,7 +72,6 @@ const DoctorDayPlannerPage = () => {
         toggleDisplay()
     }
 
-
     useEffect( () => {
         setDayOverview(initialStateDayOverview)
         const url = 'http://localhost:3001/get-doctor-appointments-for-day'
@@ -88,15 +88,15 @@ const DoctorDayPlannerPage = () => {
             .then(data => {
                 if(data.length > 0) {
                     const appointmentData = {
-                        1: {},
-                        2: {},
-                        3: {},
-                        4: {},
-                        5: {},
-                        6: {},
-                        7: {},
-                        8: {}
-                    }
+                            1: {},
+                            2: {},
+                            3: {},
+                            4: {},
+                            5: {},
+                            6: {},
+                            7: {},
+                            8: {}
+                        }
                     data.forEach(appointment => {
                         appointmentData[appointment.time_slot] = appointment
                     })
@@ -111,28 +111,105 @@ const DoctorDayPlannerPage = () => {
             <div>
                 <DatePicker setDateSelected={setDateSelected}/>
             </div>
-            {/*<table>*/}
-            {/*        <tr>*/}
-            {/*            <td>09:00</td>*/}
-            {/*            <td>{dayOverview[1].patient ? dayOverview[1].patient : ''}</td>*/}
-            {/*            <AppointmentInfoModal className={!moduleDisplay ? 'd-none' : ''}*/}
-            {/*                timeSlot={displayTimeSlot(dayOverview[1].time_slot)}*/}
-            {/*                name={dayOverview[1].patient ? dayOverview[1].patient : ''}*/}
-            {/*                reasonForVisit={dayOverview[1].reason_for_visit ? dayOverview[1].reason_for_visit : ''}*/}
-            {/*                dob={dayOverview[1].patient_dob ? dayOverview[1].patient_dob : ''}*/}
-            {/*                gender={dayOverview[1].patient_gender ? dayOverview[1].patient_gender : ''}*/}
-            {/*            />*/}
-            {/*        </tr>*/}
-            {/*</table>*/}
+            <table>
+                    <tr>
+                        <td>09:00</td>
+                        <td>{dayOverview[1].patient ? dayOverview[1].patient : ''}</td>
+                        <AppointmentInfoModal className={!moduleDisplay ? 'd-none' : ''}
+                            timeSlot={displayTimeSlot(dayOverview[1].time_slot)}
+                            name={dayOverview[1].patient ? dayOverview[1].patient : ''}
+                            reasonForVisit={dayOverview[1].reason_for_visit ? dayOverview[1].reason_for_visit : ''}
+                            dob={dayOverview[1].patient_dob ? dayOverview[1].patient_dob : ''}
+                            gender={dayOverview[1].patient_gender ? dayOverview[1].patient_gender : ''}
+                        />
+                    </tr>
+                    <tr>
+                        <td>09:00</td>
+                        <td>{dayOverview[2].patient ? dayOverview[2].patient : ''}</td>
+                        <AppointmentInfoModal className={!moduleDisplay ? 'd-none' : ''}
+                                              timeSlot={displayTimeSlot(dayOverview[2].time_slot)}
+                                              name={dayOverview[2].patient ? dayOverview[2].patient : ''}
+                                              reasonForVisit={dayOverview[2].reason_for_visit ? dayOverview[2].reason_for_visit : ''}
+                                              dob={dayOverview[2].patient_dob ? dayOverview[2].patient_dob : ''}
+                                              gender={dayOverview[2].patient_gender ? dayOverview[2].patient_gender : ''}
+                        />
+                    </tr>
+                    <tr>
+                        <td>09:00</td>
+                        <td>{dayOverview[3].patient ? dayOverview[3].patient : ''}</td>
+                        <AppointmentInfoModal className={!moduleDisplay ? 'd-none' : ''}
+                                              timeSlot={displayTimeSlot(dayOverview[3].time_slot)}
+                                              name={dayOverview[3].patient ? dayOverview[3].patient : ''}
+                                              reasonForVisit={dayOverview[3].reason_for_visit ? dayOverview[3].reason_for_visit : ''}
+                                              dob={dayOverview[3].patient_dob ? dayOverview[3].patient_dob : ''}
+                                              gender={dayOverview[3].patient_gender ? dayOverview[3].patient_gender : ''}
+                        />
+                    </tr>
+                    <tr>
+                        <td>09:00</td>
+                        <td>{dayOverview[4].patient ? dayOverview[4].patient : ''}</td>
+                        <AppointmentInfoModal className={!moduleDisplay ? 'd-none' : ''}
+                                              timeSlot={displayTimeSlot(dayOverview[4].time_slot)}
+                                              name={dayOverview[4].patient ? dayOverview[4].patient : ''}
+                                              reasonForVisit={dayOverview[4].reason_for_visit ? dayOverview[4].reason_for_visit : ''}
+                                              dob={dayOverview[4].patient_dob ? dayOverview[4].patient_dob : ''}
+                                              gender={dayOverview[4].patient_gender ? dayOverview[4].patient_gender : ''}
+                        />
+                    </tr>
+                    <tr>
+                        <td>09:00</td>
+                        <td>{dayOverview[5].patient ? dayOverview[5].patient : ''}</td>
+                        <AppointmentInfoModal className={!moduleDisplay ? 'd-none' : ''}
+                                              timeSlot={displayTimeSlot(dayOverview[5].time_slot)}
+                                              name={dayOverview[5].patient ? dayOverview[5].patient : ''}
+                                              reasonForVisit={dayOverview[5].reason_for_visit ? dayOverview[5].reason_for_visit : ''}
+                                              dob={dayOverview[5].patient_dob ? dayOverview[5].patient_dob : ''}
+                                              gender={dayOverview[5].patient_gender ? dayOverview[5].patient_gender : ''}
+                        />
+                    </tr>
+                    <tr>
+                        <td>09:00</td>
+                        <td>{dayOverview[6].patient ? dayOverview[6].patient : ''}</td>
+                        <AppointmentInfoModal className={!moduleDisplay ? 'd-none' : ''}
+                                              timeSlot={displayTimeSlot(dayOverview[6].time_slot)}
+                                              name={dayOverview[6].patient ? dayOverview[6].patient : ''}
+                                              reasonForVisit={dayOverview[6].reason_for_visit ? dayOverview[6].reason_for_visit : ''}
+                                              dob={dayOverview[6].patient_dob ? dayOverview[6].patient_dob : ''}
+                                              gender={dayOverview[6].patient_gender ? dayOverview[6].patient_gender : ''}
+                        />
+                    </tr>
+                    <tr>
+                        <td>09:00</td>
+                        <td>{dayOverview[7].patient ? dayOverview[7].patient : ''}</td>
+                        <AppointmentInfoModal className={!moduleDisplay ? 'd-none' : ''}
+                                              timeSlot={displayTimeSlot(dayOverview[7].time_slot)}
+                                              name={dayOverview[7].patient ? dayOverview[7].patient : ''}
+                                              reasonForVisit={dayOverview[7].reason_for_visit ? dayOverview[7].reason_for_visit : ''}
+                                              dob={dayOverview[7].patient_dob ? dayOverview[7].patient_dob : ''}
+                                              gender={dayOverview[7].patient_gender ? dayOverview[7].patient_gender : ''}
+                        />
+                    </tr>
+                    <tr>
+                        <td>09:00</td>
+                        <td>{dayOverview[8].patient ? dayOverview[8].patient : ''}</td>
+                        <AppointmentInfoModal className={!moduleDisplay ? 'd-none' : ''}
+                                              timeSlot={displayTimeSlot(dayOverview[8].time_slot)}
+                                              name={dayOverview[8].patient ? dayOverview[8].patient : ''}
+                                              reasonForVisit={dayOverview[8].reason_for_visit ? dayOverview[8].reason_for_visit : ''}
+                                              dob={dayOverview[8].patient_dob ? dayOverview[8].patient_dob : ''}
+                                              gender={dayOverview[8].patient_gender ? dayOverview[8].patient_gender : ''}
+                        />
+                    </tr>
+            </table>
 
-            {
-                dayOverview.forEach(appointment => {
-                <table>
-                    <TableRow appointment={appointment} displayTimeSlot={displayTimeSlot} moduleDisplay={moduleDisplay} />
-                </table>
-            })
-            }
-
+            {/*{*/}
+            {/*    dayOverview[0].forEach(appointment => {*/}
+            {/*        <table>*/}
+            {/*            <TableRow appointment={appointment} displayTimeSlot={displayTimeSlot}*/}
+            {/*                      moduleDisplay={moduleDisplay}/>*/}
+            {/*        </table>*/}
+            {/*    })*/}
+            {/*}*/}
         </div>
     )
 }
